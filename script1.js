f0f71d5566a4bb023936351fa9c0fa8704839796
Mikhail Yakunin <yauser@mail.ru>
2022-07-04 13:55:53 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,29 @@
+/* const result = confirm("How are you?"); */
+
+/* let namber = 5;
+const leftBorderWidth = 1; */
+/* console.log(result); */
+/* const answer = prompt("Вам есть 18?", "21");
+console.log(answer); */
+/* const answers = [];
+
+answers[0] = prompt("Как тебя зовут?", "");
+answers[1] = prompt("Как твоя фамилия?", "");
+answers[2] = prompt("Сколько тебе лет?", "");
+
+console.log(typeof(answers));
+;
+document.write(answers); */
+
+
+/*     const category = "toys";
+    
+    console.log(`https://some.org/${category}/5`); */
+
+/*     const isChecked = false,
+          isClosed = true;
+
+console.log(isChecked || isClosed); */
+ 
+
+
