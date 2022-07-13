79b9a5a0adc56e3989dcea55758f30369e8e2380
Mikhail Yakunin <yauser@mail.ru>
2022-07-13 11:13:58 +0300

    five commit

--- Project_1_study/script.js
+++ Project_1_study/script.js
@@ -1,5 +1,14 @@
 "use strict";
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,32 +18,68 @@ const personalMovieDB = {
     private: false
 };
  
-for (let i = 0; i<2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов", ""),
-          b = prompt("На сколько оцените его?", "");
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-} else {
-    console.log("error");
-    i--;
-}
-}
 
-
-/* if (personalMovieDB.count < 10) {
-    console.log("Просмотрено слишком мало фильмов.");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель.");
-    } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман.");
+function rememberMyFilms() {
+    for (let i = 0; i<2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов", ""),
+              b = prompt("На сколько оцените его?", "");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
     } else {
-    console.log("Произощла ошибка.");
+        console.log("error");
+        i--;
+    }
+    }
+}
 
-} */
-(personalMovieDB.count < 10) ? console.log("Просмотрено слишком мало фильмов.") : ((personalMovieDB.count >= 10 && personalMovieDB.count < 30) ? console.log("Вы классический зритель.") : ((personalMovieDB.count >= 30)? console.log("Вы киноман.") : console.log("Произощла ошибка.")));
+rememberMyFilms();
 
-console.log(personalMovieDB);
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено слишком мало фильмов.");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель.");
+        } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман.");
+        } else {
+        console.log("Произощла ошибка.");
+    
+    }
+}
+
+detectPersonalLevel();
+
+ function showMyDB(hidden) {
+     if (!hidden) {
+         console.log(personalMovieDB);         
+     }
+ }
+
+ showMyDB(personalMovieDB.private);
+
+ function writeYourGenres() {
+    for (let i = 1; i<=3; i++) {
+        const a = prompt(`Ваш любимый жанр под номером ${i}`);
+        if (a != null && a != '' && a.length < 50) {
+            personalMovieDB.gernes[i - 1] = a;
+            console.log('done');
+    } else {
+        console.log("error");
+        i--;
+    }
+    }
+ }
+
+ writeYourGenres();
+
+
+
+/* (personalMovieDB.count < 10) ? console.log("Просмотрено слишком мало фильмов.") : ((personalMovieDB.count >= 10 && personalMovieDB.count < 30) ? console.log("Вы классический зритель.") : ((personalMovieDB.count >= 30)? console.log("Вы киноман.") : console.log("Произощла ошибка.")));
+
+console.log(personalMovieDB); */
 
 
 /* numberOfFilms[0] = prompt("Сколько фильмов Вы уже посмотрели?", "");
--- Project_1_study/script1.js
+++ Project_1_study/script1.js
@@ -24,13 +24,238 @@ document.write(answers);
           isClosed = true;
 
 console.log(isChecked || isClosed); */ 
- const numberOfFilms = confirm(Сколько фильмов Вы уже посмотрели?);
- const count = [numberOfFilms];
- const movies = [];
- const actors = [];
- const gernes {
 
+/* ФУНКЦИИ */
+
+let num = 20;
+
+ function showFirsMessage(text) {
+     console.log(text);
+     let num = 10;
+ }
+showFirsMessage("Hellow World!");
+console.log(num);
+
+function calc(a, b) {
+return (a + b);
+}
+console.log(calc(2, 4));
+console.log(calc(3, 7));
+console.log(calc(4, 8));
+console.log(calc(10, 15));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+const anotherNum =ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello!");
+}
+logger();
+
+const calc = (a, b) => { return a + b }
+console.log(calc(2, 4));
+/* КОНЕЦ УРОКА ФУНКЦИИ */
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const logg = "Hello world";
+/* console.log(logg.substr(6, 5)); */
+console.log(logg.slice(6));
+
+/* КОЛБЭК ФУНКЦИИ */
+
+function first() {
+    // Do samething
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+console.log(`Я учу ${lang}`);
+callback();
+}
+function done() {
+    console.log("Я прошёл этот урок!")
+};
+
+learnJS("JavaScript", done);
+/* КОНЕЦ УРОКА О КОЛБЭК ФУНКЦИЯХ */
+
+/* ОБЪЕКТЫ */
+const options = {
+    name: 'Test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+options.makeTest();
+const {border, bg} = options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key])=== 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        } 
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+        }
+    }
+    
+    console.log(counter);
+
+/*     КОНЕЦ УРОКА ПРО ОБЪЕКТЫ */
+
+/* 021 МФССИВЫ И ПСЕВДОМАССИВЫ     */
+
+const arr = [2,34, 16, 8, 10];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+/* arr[99] = 0;
+console.log(arr); */
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} */
+
+/* for (let value of arr) {
+    console.log(value);
+} */
+
+arr.forEach (function(item, i, arr) {
+    console.log(`${i}: ${item} в массиве ${arr}`);
+});
+
+const str = prompt("", "");
+const product = str.split(",");
+console.log(product);
+
+/* КОНЕЦ УРОКА ПРО МАССИВЫ */
+
+/* ПЕРЕДАЧА ПО ССЫЛКЕ ИЛИ ПО ЗНАЧЕНИЮ */
+
+let a = 5,
+    b = a;
+
+b = a + b;
+ console.log(b);
+ console.log(a);
+
+/*  const obj = {
+    a: 5,
+    b: 1
  };
 
+ const copy = obj;
+ copy.a = 10;
 
+ console.log(copy.a);
+ console.log(obj.a); */
+
+ function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+ }
+ 
+ const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+ };
+
+ const newNumbers = copy(numbers);
+ newNumbers.a = 10;
+
+ /* console.log(newNumbers);
+ console.log(numbers); */
+ newNumbers.c.x = 20;
+
+/*  console.log(newNumbers);
+ console.log(numbers); */
+
+ const add = {
+    d: 17,
+    e: 25
+ };
+
+/*  console.log(Object.assign(numbers, add)); */
+
+const clone = Object.assign({}, add);
+
+clone.d = 23;
+
+console.log(clone);
+console.log(add);
+
+const oldArray = ["a", "b", "c"];
+
+const newArray = oldArray.slice();
+
+newArray[1] = "fghd";
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blog = ['worlpress', 'livejournal', 'blogger'],
+      internet =[...video, ...blog, 'vk', 'facebook'];
+      
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [1, 2, 3];
+ log(...num);
+
+ const array = ["a", "b"];
+
+ const newAarray = [...array];
+
+ const q = {
+    one: 1,
+    two: 2
+ };
+
+ const newObl = {...q};
+
+ /* КОНЕЦ УРОКА ПЕРЕДАЧА ПО ССЫЛКЕ */
 
